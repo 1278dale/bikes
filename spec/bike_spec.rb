fff849404309b09e3e3f0755cc412c0cf0428dc0
1278dale <dalestevens1278@gmail.com>
2014-10-31 16:51:03 +0000

    first example for bike class and test file

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,11 +1,14 @@
 # bike should not be broken when created
 # bike should be able to break
 # bike should be able to be fixed
+require 'bike'
 
 describe Bike do
-  bike = Bike.new
-  expect().to_be
-  
+
+  it "should not be broken when creating a new bike" do
+    bike = Bike.new
+    expect(bike.broken?).to be false
+  end
 end
 
 
