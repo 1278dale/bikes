49627554729a1b8a3f503630c96df797ec35637e
1278dale <dalestevens1278@gmail.com>
2014-10-31 15:40:12 +0000

    Emptied bike and station spec files

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,27 +1,7 @@
 # bike should not be broken when created
 # bike should be able to break
 # bike should be able to be fixed
-require 'bike'
 
-describe Bike do
-
-  let(:bike) { Bike.new }
-  
-  it "should not be broken when it is created" do
-  expect(bike.broken?).to eq false
-  end
-
-  it "should be able to break" do 
-    bike.break!
-    expect(bike.broken?).to eq true
-  end
-
-  it "should be able to be fixed" do
-    bike.fix!
-    expect(bike.broken?).to eq false
-  end
-
-end
 
 
 
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -3,30 +3,3 @@
 # should have a capacity limit and know it
 # should not accept broken bikes
 # must know how many bikes it has
-require 'station'
-
-describe Station do
-
-  let(:bike) {Bike.new}
-  let(:station) {Station.new}(:capacity => 20)
-
-  it "should be able to receive bikes" do
-  expect(station.bike_count).to eq(0)
-  station.store(bike)
-  expect(station.bike_count).to eq(1)
-  end
-
-  it "should be able to release a bike" do
-    station.store(bike)
-    expect(station.bike_count).to eq(1)
-    station.release(bike)
-    expect(station.bike_count).to eq(0)
-  end
-
-  it "should know when full" do
-    expect(station).not_to be_full
-    20.times { station.dock(Bike.new)}
-    expect(station).to be_full
-  end
-
-end
